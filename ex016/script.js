f815b4d44fa96492da257a7c0310cbef7ccac44e
Thiago Alves de Souza <thiagosouza.contato01@gmail.com>
2023-08-01 12:26:53 -0300

    att

---
+++ ex016/script.js
@@ -0,0 +1,10 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+        window.alert('[ERRO] Faltam dados!')
+    }
+   }
