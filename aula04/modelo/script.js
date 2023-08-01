cde273d50de6ed16657959c6992b2f5265ffca6e
Thiago Alves de Souza <thiagosouza.contato01@gmail.com>
2023-08-01 11:36:36 -0300

    att

--- aula04/modelo/script.js
+++ aula04/modelo/script.js
@@ -6,5 +6,17 @@ function verificar(){
     
    if(fano.value.lenght == 0 || fano.value > ano) {
     alert('[ERRO] Verifique os dados e tente novamente!')
+   } else {
+    var fsex = document.getElementByName('radsex')
+    var idade = ano - Number(fano.value)
+    var gênero = ''
+    
+    if (fsex[0].checked){
+        gênero = 'Homem'
+    }else if (fsex[1].checked){
+        gênero = 'Mulher'
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `${gênero} com ${idade} anos.`
    }
 }
