e2ce442b263aafa218b6ffec60fe83b7a7a17b8d
Thiago Alves de Souza <thiagosouza.contato01@gmail.com>
2023-08-02 12:39:19 -0300

    Create index.js

---
+++ aula03/index.js
@@ -0,0 +1,20 @@
+console.log('Vai começar...')
+for (var c = 1;c <= 3; c++){
+    console.log(`passo ${c}`)
+}
+console.log('Fim!')
+
+
+/*
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 2)
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
