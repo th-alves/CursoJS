2d7690536c2a4b635105788131db54e384222bcc
Thiago Alves de Souza <thiagosouza.contato01@gmail.com>
2023-08-08 12:49:30 -0300

    Create ambiente.js

---
+++ ex018/ambiente.js
@@ -0,0 +1,18 @@
+let valores = [1,2,3,4,5,6]
+
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}//para cada posição em valores, eu vou mostrar o valores pos
+
+
+/*for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+let num = [2, 3, 2, 1, 2]
+
+num.push(4)
+num.sort()
+console.log(num)
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O primeiro número é o ${num[0]}`)*/
