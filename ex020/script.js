5913a92593caca242be068e66ddfd96287e62462
Thiago Alves de Souza <thiagosouza.contato01@gmail.com>
2023-08-12 11:21:49 -0300

    att

--- ex020/script.js
+++
@@ -1,9 +0,0 @@
-let num = document.querySelector('input#fnum')
-let lista = document.querySelector('select#flista')
-let res = document.querySelector('div#res')
-let valores = []
-
-
-function adicionar(){
-    if(isNumero(num.value))
-}
---
+++ trybe-js/aula01.js
@@ -0,0 +1,1 @@
+let comidaFavorita = frango
